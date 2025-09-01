34283153cec7cb93d00f883257e69e0deae8490e
yoghurt <git@yoggi.club>
2025-09-01 12:44:07 +0200

    Tetgen viewer

---
+++ flake.nix
@@ -0,0 +1,69 @@
+{
+  description = "A Nix-flake-based C/C++ development environment";
+
+  #inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+
+  outputs =
+    inputs:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import inputs.nixpkgs { inherit system; };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default =
+            pkgs.mkShell.override
+              {
+                # Override stdenv in order to change compiler:
+                # stdenv = pkgs.clangStdenv;
+              }
+              {
+                packages =
+                  with pkgs;
+                  [
+                    clang-tools
+                    cmake
+                    codespell
+                    conan
+                    cppcheck
+                    doxygen
+                    gtest
+                    lcov
+                    vcpkg
+                    vcpkg-tool
+
+		    # libigl dependencies:
+		    glfw
+		    xorg.libX11
+		    xorg.libXrandr
+		    xorg.libXinerama
+		    xorg.libXcursor
+		    xorg.libXi
+                  ]
+                  ++ (if system == "aarch64-darwin" then [ ] else [ gdb ]);
+
+                # Set up library paths for X11 libraries
+                shellHook = ''
+                  export LD_LIBRARY_PATH="${pkgs.xorg.libX11}/lib:$LD_LIBRARY_PATH"
+                  echo "X11 libraries configured. If running on Wayland, start Xwayland with 'Xwayland :10' and set 'export DISPLAY=:10'"
+                '';
+              };
+        }
+      );
+    };
+}
--- main.cpp
+++ main.cpp
@@ -1,34 +1,127 @@
 #include <igl/opengl/glfw/Viewer.h>
+#include <igl/readOBJ.h>
+#include <igl/copyleft/tetgen/tetrahedralize.h>
+#include <igl/barycenter.h>
+#include <igl/stb/read_image.h>
+#include <iostream>
+
+// Input mesh
+Eigen::MatrixXd V;
+Eigen::MatrixXi F;
+
+
+// Tetrahedralized interior
+Eigen::MatrixXd TV;
+Eigen::MatrixXi TT;
+Eigen::MatrixXi TF;
+Eigen::MatrixXd Bc;
+
+// tetrahedra diplay triangles
+Eigen::MatrixXd dV;
+Eigen::MatrixXi dF;
+
+// matcap texture
+Eigen::Matrix<unsigned char,Eigen::Dynamic,Eigen::Dynamic> R,G,B,A;
+
+bool displayTetrahedralMesh = true;
+
+
+void display_tetrahedra(igl::opengl::glfw::Viewer& viewer, unsigned char key)
+{
+  using namespace std;
+  using namespace Eigen;
+
+  if (key < '1' && key > '9')
+  {
+    return;
+  }
+
+  double t = double((key - '1')+1) / 9.0;
+
+  VectorXd v = Bc.col(2).array() - Bc.col(2).minCoeff();
+  v /= v.col(0).maxCoeff();
+
+  vector<int> s;
+
+  for (unsigned i=0; i<v.size();++i)
+    if (v(i) < t)
+      s.push_back(i);
+
+  dV = MatrixXd(s.size()*4,3);
+  dF = MatrixXi(s.size()*4,3);
+
+  for (unsigned i=0; i<s.size();++i)
+  {
+    dV.row(i*4+0) = TV.row(TT(s[i],0));
+    dV.row(i*4+1) = TV.row(TT(s[i],1));
+    dV.row(i*4+2) = TV.row(TT(s[i],2));
+    dV.row(i*4+3) = TV.row(TT(s[i],3));
+    dF.row(i*4+0) << (i*4)+0, (i*4)+1, (i*4)+3;
+    dF.row(i*4+1) << (i*4)+0, (i*4)+2, (i*4)+1;
+    dF.row(i*4+2) << (i*4)+3, (i*4)+2, (i*4)+0;
+    dF.row(i*4+3) << (i*4)+1, (i*4)+2, (i*4)+3;
+  }
+
+  viewer.data().clear();
+  viewer.data().set_mesh(dV,dF);
+  viewer.data().set_face_based(true);
+  viewer.data().set_texture(R,G,B,A);
+  viewer.data().use_matcap = true;
+}
+
+
+void change_mesh(igl::opengl::glfw::Viewer& viewer) 
+{
+    displayTetrahedralMesh = !displayTetrahedralMesh;
+    viewer.data().clear();
+
+    if(displayTetrahedralMesh)
+    {
+      viewer.data().set_mesh(dV, dF);
+    }
+    else 
+    {
+      viewer.data().set_mesh(V,F);
+    }
+    viewer.data().set_texture(R,G,B,A);
+    viewer.data().use_matcap = true;
+    viewer.data().set_face_based(true);
+}
+
+
+bool key_down(igl::opengl::glfw::Viewer& viewer, unsigned char key, int modifier)
+{
+  std::cout << "Input: " << "0x" << std::hex << static_cast<unsigned int>(static_cast<unsigned char>(key)) << std::endl;
+
+  if (key == 0x20) {
+    change_mesh(viewer);
+  }
+  if (key >= '1' && key <= '9'){
+    display_tetrahedra(viewer, key);
+  }
+  return false;
+}
 
 int main(int argc, char *argv[])
 {
-  // Inline mesh of a cube
-  const Eigen::MatrixXd V= (Eigen::MatrixXd(8,3)<<
-    0.0,0.0,0.0,
-    0.0,0.0,1.0,
-    0.0,1.0,0.0,
-    0.0,1.0,1.0,
-    1.0,0.0,0.0,
-    1.0,0.0,1.0,
-    1.0,1.0,0.0,
-    1.0,1.0,1.0).finished();
-  const Eigen::MatrixXi F = (Eigen::MatrixXi(12,3)<<
-    0,6,4,
-    0,2,6,
-    0,3,2,
-    0,1,3,
-    2,7,6,
-    2,3,7,
-    4,6,7,
-    4,7,5,
-    0,4,5,
-    0,5,1,
-    1,5,7,
-    1,7,3).finished();
+  if(!igl::readOBJ("../meshes/spot_triangulated.obj", V, F)){
+    std::cout << "Failed to load obj\n";
+  }
 
+  // Tetrahedralize the interior
+  igl::copyleft::tetgen::tetrahedralize(V,F,"pq1.414Y", TV,TT,TF);
+
+  // Compute barycenters
+  igl::barycenter(TV,TT,Bc);
+
+  // Add matcap
+  igl::stb::read_image("../matcap/ceramic_dark.png", R,G,B,A); 
+
+  
   // Plot the mesh
   igl::opengl::glfw::Viewer viewer;
-  viewer.data().set_mesh(V, F);
-  viewer.data().set_face_based(true);
+  viewer.core().background_color.setConstant(0.3f);
+  viewer.callback_key_down = &key_down;
+  key_down(viewer, '5', 0);
   viewer.launch();
 }
